99f7878b45e33b3afb6165f8f78333c9858ed60a
Guillaume Manen <g.manen@c2is.fr>
2014-06-19 15:47:32 +0200

    Initial commit

---
+++ BigfootDefaultThemeBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bigfoot\Theme\BigfootDefaultThemeThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class BigfootContentBundle
+ *
+ * @Author S.huot s.huot@c2is.fr
+ * @package Bigfoot\Bundle\ContentBundle
+ */
+class BigfootDefaultThemeBundle extends Bundle
+{
+}
